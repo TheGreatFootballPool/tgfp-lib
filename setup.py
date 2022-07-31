2ffb0bdd4bbb7489b10cf71c3e60506d6a92636e
John Sturgeon <john.sturgeon@me.com>
2022-07-31 10:27:00 -0700

    Updated setup file and prepared for tag v1.0.1

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ with open('requirements.txt', encoding='utf8') as f:
     requirements = f.readlines()
 
 setup(
-    name='yahoo-nfl',
-    version='1.0.0',
-    packages=['yahoo_nfl'],
+    name='tgfp-lib',
+    version='1.0.1',
+    packages=['tgfp_lib'],
     python_requires='>=3.10.*',
-    url='https://github.com/johnsturgeon/yahoo-nfl',
+    url='https://github.com/johnsturgeon/tgfp-lib',
     license='MIT',
     author='John Sturgeon',
     author_email='john.sturgeon@me.com',
     install_requires=requirements,
-    description='Python extraction of NFL scores schedule data from Yahoo',
+    description='Python library for The Great Football Pool',
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
